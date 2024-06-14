ad67604f24db5657501e8659d51fed54e9a41e26
Robin Lemaire <robin.lemaire@adevinta.com>
2024-06-14 17:51:18 +0200

    Migrate from spark-ios

---
+++ Sources/SparkButton/UseCase/GetState/ButtonGetStateUseCase.swift
@@ -0,0 +1,33 @@
+//
+//  ButtonGetStateUseCase.swift
+//  SparkButton
+//
+//  Created by robin.lemaire on 27/06/2023.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import SparkTheming
+
+// sourcery: AutoMockable, AutoMockTest
+protocol ButtonGetStateUseCaseable {
+    // sourcery: dims = "Identical"
+    func execute(isEnabled: Bool,
+                 dims: Dims) -> ButtonState
+}
+
+struct ButtonGetStateUseCase: ButtonGetStateUseCaseable {
+
+    // MARK: - Methods
+
+    func execute(
+        isEnabled: Bool,
+        dims: Dims
+    ) -> ButtonState {
+        let opacity = isEnabled ? dims.none : dims.dim3
+
+        return .init(
+            isUserInteractionEnabled: isEnabled,
+            opacity: opacity
+        )
+    }
+}
---
+++ Sources/SparkButton/ViewModel/Button/ButtonSUIViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  ButtonSUIViewModel.swift
+//  SparkButton
+//
+//  Created by robin.lemaire on 15/01/2024.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import SwiftUI
+@_spi(SI_SPI) import SparkCommon
+import SparkTheming
+
+final class ButtonSUIViewModel: ButtonViewModel, ButtonMainSUIViewModel {
+
+    // MARK: - Properties
+
+    var controlStatus: ControlStatus = .init()
+
+    // MARK: - Published Properties
+
+    @Published private(set) var controlStateImage: ControlStateImage = .init()
+    @Published private(set) var controlStateText: ControlStateText? = .init()
+    @Published var maxWidth: CGFloat?
+    @Published var frameAlignment: Alignment = .center
+
+    // MARK: - Initialization
+
+    init(
+        theme: Theme,
+        intent: ButtonIntent,
+        variant: ButtonVariant,
+        size: ButtonSize,
+        shape: ButtonShape,
+        alignment: ButtonAlignment
+    ) {
+        super.init(
+            for: .swiftUI,
+            theme: theme,
+            intent: intent,
+            variant: variant,
+            size: size,
+            shape: shape,
+            alignment: alignment
+        )
+    }
+}
