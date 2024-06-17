81f51ce7dce201d8d476fb20e0269aa03b5a01db
Robin Lemaire <robin.lemaire@adevinta.com>
2024-06-17 11:40:58 +0200

    Migrate from spark-ios

---
+++ Sources/SparkButtonTesting/Properties/Internal/State/ButtonState+ExtensionTests.swift
@@ -0,0 +1,25 @@
+//
+//  ButtonState.swift
+//  SparkButton
+//
+//  Created by robin.lemaire on 27/06/2023.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import Foundation
+@testable import SparkButton
+
+extension ButtonState {
+
+    // MARK: - Properties
+
+    static func mocked(
+        isUserInteractionEnabled: Bool = true,
+        opacity: CGFloat = 1.0
+    ) -> Self {
+        return .init(
+            isUserInteractionEnabled: isUserInteractionEnabled,
+            opacity: opacity
+        )
+    }
+}
